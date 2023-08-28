cbb17e374889978c0a4bbf4be0fff954e0781429
abhishek <abhisingh562002offc@gmail.com>
2023-08-28 23:31:39 +0530

    almost finish 3rd project

---
+++ 3QuizApp/script.js
@@ -0,0 +1,96 @@
+const questions = [
+  {
+    question: "How many planets are there in our milky way?",
+    answers: [
+      { text: "5", correct: false },
+      { text: "2", correct: false },
+      { text: "7", correct: false },
+      { text: "Infinite", correct: true },
+    ],
+  },
+  {
+    question: "Which is the largest country in the world?",
+    answers: [
+      { text: "India", correct: false },
+      { text: "Russia", correct: true },
+      { text: "USA", correct: false },
+      { text: "Australia", correct: false },
+    ],
+  },
+  {
+    question: "How many moons does mars have?",
+    answers: [
+      { text: "5", correct: false },
+      { text: "2", correct: true },
+      { text: "7", correct: false },
+      { text: "10+", correct: true },
+    ],
+  },
+  {
+    question: "Which is the farthest planet from our Sun?",
+    answers: [
+      { text: "Neptune", correct: true },
+      { text: "Pluto", correct: false },
+      { text: "Venus", correct: false },
+      { text: "Uranus", correct: true },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButton.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+startQuiz();
